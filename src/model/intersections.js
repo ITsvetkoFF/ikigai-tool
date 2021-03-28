4cb95977854db634f3abe51be5b9d8b71c58a030
Yaroslav Samoilenko <YSamoilenko@otalio.com>
2021-03-28 18:49:10 +0300

    Implemented intersections: same logic as pillars with the same text/zooming idea

---
+++ src/model/intersections.js
@@ -0,0 +1,6 @@
+export const intersections = [
+    {name: "PASSION", title: "Your passion", description: "Love and what you're good at", position: 1},
+    {name: "MISSION", title: "Your mission", description: "Love and world's need", position: 2},
+    {name: "PROFESSION", title: "Your profession", description: "Good at and paid for", position: 3},
+    {name: "VOCATION", title: "Your vocation", description: "World's need and paid for", position: 4},
+];
